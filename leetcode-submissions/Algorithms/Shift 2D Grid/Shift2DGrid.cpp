c67ed0f1f3d739d5b24883a73a3b73ce55a60ca9
Rohit Prasad <73442980+rohitprasad-code@users.noreply.github.com>
2026-07-22 06:50:07 +0530

    Sync: Shift 2D Grid (cpp)

---
+++ leetcode-submissions/Algorithms/Shift 2D Grid/Shift2DGrid.cpp
@@ -0,0 +1,40 @@
+/*
+  LeetCode Submission ID: 2076459074
+  Date: 22/07/2026, 06:50:06
+  Runtime: 3 ms
+  Memory: 18.1 MB
+  Difficulty: Easy
+  Problem Link: https://leetcode.com/problems/shift-2d-grid/submissions/2076459074/?envType=daily-question&envId=2026-07-20
+*/
+
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int total = m * n;
+        k %= total;
+
+        vector<vector<int>> ans(m, vector<int>(n));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                // index in 1D array (before rotation)
+                int oldIndex = i * n + j;
+
+                // index in 1D array (after rotation)
+                int newIndex = (oldIndex + k) % total;
+
+                // changing from 1d back to 2D
+                int newRow = newIndex / n;
+                int newCol = newIndex % n;
+
+                ans[newRow][newCol] = grid[i][j];
+            }
+        }
+
+        return ans;
+    }
+};
